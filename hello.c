cb8df76974040c3a2043cf0d665e5d4353494dc6
Refone <hustliuruifeng@gmail.com>
2017-11-22 20:34:45 +0800

    filename

--- hello.c
+++ hello.c
@@ -1,9 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define MMAP_NUM 100
+
+char *filename(int i)
+{
+    static char name[10];
+
+    sprintf(name, "%d", i);
+
+    return name;
+}
 
 int main()
 {
-    printf("this is hello.\n");
+    printf("This is page fault creator.\n");
+
+    int i;
+    FILE *fp[MMAP_NUM];
+
+    for (i=0; i<MMAP_NUM; i++) {
+        //fp[i] = fopen();
+        printf("%s ", filename(i));
+    }
+    
+    printf("\n");
 
     return 0;
 }
